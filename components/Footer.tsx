c23e3d87d6ef7668fcc0a9d49006562d16838c1a
tousen1313 <a09025340241@gmail.com>
2022-11-21 21:48:15 +0900

    init commit

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+import { BadgeCheckIcon } from '@heroicons/react/solid'
+
+export const Footer = () => {
+  return (
+    <footer className="flex h-12 w-full items-center justify-center border-t">
+    フッター
+    <BadgeCheckIcon className="h-6 w-6 text-blue-500" />
+  </footer>
+  )
+}
